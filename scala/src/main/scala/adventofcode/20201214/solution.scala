522e834c7388be7293981e866be18576828fa859
hyperchessbot <hyperchessbot@gmail.com>
2020-12-13 21:07:02 +0000

    Get package name

--- scala/src/main/scala/adventofcode/20201214/solution.scala
+++ scala/src/main/scala/adventofcode/20201214/solution.scala
@@ -4,12 +4,14 @@ import scala.language.postfixOps
 import utils._
 import table._
 
-object problem{
-	val prefix = "src/main/scala/adventofcode/20201214/"
+object problem{	
+	val packageDate = "([0-9]+)".r.findFirstMatchIn(problem.getClass.getName).get
+	
+	val prefix = s"src/main/scala/adventofcode/$packageDate/"
 	
 	def solveInput(input:Tuple2[String, Int]):Unit = {
 		val lines = getLinesOf(s"$prefix${input._1}.txt")
-	
+			
 		
 	}
 	
@@ -17,7 +19,7 @@ object problem{
 		val delim = "----------------------"
 		
 		for(input <- List(("example", 0), ("input", 0), ("example", 1), ("input", 1))) time(input._1 + " version " + input._2.toString(), "ms", {    
-			println (s"$delim\n${input._1} version ${input._2} processing")
+			println (s"$delim\n$packageDate ${input._1} version ${input._2} processing")
 			
 			solveInput(input)
 		})
