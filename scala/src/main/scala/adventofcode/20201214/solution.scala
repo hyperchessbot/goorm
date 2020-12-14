aeb3ca9200a585d0361382eb355435c4741e90e9
scalasbtprojects <scalasbtprojects@gmail.com>
2020-12-14 08:33:22 +0100

    Day 14 Part 2

--- scala/src/main/scala/adventofcode/20201214/solution.scala
+++ scala/src/main/scala/adventofcode/20201214/solution.scala
@@ -25,39 +25,70 @@ object problem{
 		val arg = parts(1)
 		var andMask:Long = (2L << memSize) - 1
 		var orMask:Long = 0
+		var floatMask:Long = 0
+		var floatDisableMask:Long = (2L << memSize) - 1
 		var address:Long = 0
 		var value:Long = 0
 		command match {
 			case "mask" => {
 				andMask = parseLong(arg.replaceAll("X|1", "1"))
 				orMask = parseLong(arg.replaceAll("X|0", "0"))
+				floatMask = parseLong(arg.replaceAll("1|0", "0").replaceAll("X", "1"))				
+				floatDisableMask = parseLong(arg.replaceAll("1|0", "1").replaceAll("X", "0"))				
 			}
 			case _ => {
 				address = command.split("\\[|\\]")(1).toLong
 				value = arg.toLong				
 			}
 		}
-		println(arg)
-		println(andMask.toBinaryString)
-		println(orMask.toBinaryString)
 	}
 	
-	case class Machine(instructions:List[Instruction]){
+	case class Machine(instructions:List[Instruction], v:Int){
 		val memory = scala.collection.mutable.Map[Long, Long]()
 		
 		var andMask:Long = ( 2L << memSize ) - 1
 		var orMask:Long = 0
+		var floatMask:Long = 0
+		var floatDisableMask:Long = 0
 		
 		def execInstr(ins:Instruction):Unit = {
-			ins.command match {
-				case "mask" => {
-					andMask = ins.andMask
-					orMask = ins.orMask
+			v match {
+				case 0 => ins.command match {
+					case "mask" => {
+						andMask = ins.andMask
+						orMask = ins.orMask						
+					}
+					case _ => {
+						memory(ins.address) = ins.value & andMask | orMask
+					}
 				}
-				case _ => {
-					memory(ins.address) = ins.value & andMask | orMask
+				case _ => ins.command match {
+					case "mask" => {
+						floatMask = ins.floatMask						
+						floatDisableMask = ins.floatDisableMask
+						orMask = ins.orMask
+					}
+					case _ => {
+						val memMap = scala.collection.mutable.Map[Int, Long]()
+						var index = 0
+						for(i <- 0 until memSize){
+							val mask = 1L << i
+							if((floatMask & mask) != 0){
+								memMap(index) = mask
+								index += 1
+							}
+						}											
+						for(i <- 0 until 2 << memMap.size){							
+							var address = ins.address & floatDisableMask | orMask
+							for(index <- 0 until memMap.size){								
+								if((i & (2 << index)) != 0) address = address | memMap(index)
+							}														
+							memory(address) = ins.value
+						}
+					}
 				}
 			}
+			
 		}
 		
 		instructions.foreach(execInstr(_))
@@ -70,7 +101,7 @@ object problem{
 			
 		val instructions = lines.map(Instruction(_))
 		
-		val machine = Machine(instructions)
+		val machine = Machine(instructions, input._2)
 		
 		println(machine.sum)
 	}
@@ -78,7 +109,7 @@ object problem{
 	def solve():Unit = {
 		val delim = "----------------------"
 		
-		for(input <- List(("example", 0), ("input", 0)/*, ("example", 1), ("input", 1)*/)) time(input._1 + " version " + input._2.toString(), "ms", {    
+		for(input <- List(("example", 0), ("input", 0), ("example2", 1), ("input", 1))) time(input._1 + " version " + input._2.toString(), "ms", {    
 			println (s"$delim\n$packageDate ${input._1} version ${input._2} processing")
 			
 			solveInput(input)
