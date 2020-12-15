c60db15e22bfb8e3559c2c64ef32dfc413dc494a
hyperchessbot <hyperchessbot@gmail.com>
2020-12-15 07:17:39 +0000

    Day 15

--- scala/src/main/scala/adventofcode/20201215/solution.scala
+++ scala/src/main/scala/adventofcode/20201215/solution.scala
@@ -14,13 +14,33 @@ object problem{
 	def solveInput(input:Tuple2[String, Int]):Unit = {
 		val lines = getLinesOf(s"$prefix${input._1}.txt")
 			
-		
+		//lines.foreach(line =>
+			val nums = lines.last.split(",").map(_.toInt)
+			var spoken = nums.zipWithIndex.map(item => item._1 -> List(item._2)).toMap
+			var lastSpoken = nums.last
+			
+			for(i <- nums.length until (if(input._2 == 0) 2020 else 30000000)){				
+				val sl = spoken(lastSpoken)
+				
+				if(sl.length > 1){
+					lastSpoken = sl.last - sl.head
+				}else{
+					lastSpoken = 0
+				}
+				
+				val nl = if(spoken.contains(lastSpoken)) List(spoken(lastSpoken).last, i) else List(i)
+				
+				spoken = spoken + ( lastSpoken -> nl )
+			}
+			
+			println(lastSpoken)
+		//})
 	}
 	
 	def solve():Unit = {
 		val delim = "----------------------"
 		
-		for(input <- List(("example", 0), ("input", 0), ("example2", 1), ("input", 1))) time(input._1 + " version " + input._2.toString(), "ms", {    
+		for(input <- List(/*("example", 0), */("input", 0), /*("example2", 1), */("input", 1))) time(input._1 + " version " + input._2.toString(), "ms", {    
 			println (s"$delim\n$packageDate ${input._1} version ${input._2} processing")
 			
 			solveInput(input)
