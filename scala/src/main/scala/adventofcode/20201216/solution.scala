b984a20e8ac64ad0c69c52e1ef492e0cddbd8f7c
hyperchessbot <hyperchessbot@gmail.com>
2020-12-16 04:55:13 +0000

    Structure inputs

--- scala/src/main/scala/adventofcode/20201216/solution.scala
+++ scala/src/main/scala/adventofcode/20201216/solution.scala
@@ -14,16 +14,19 @@ object problem{
 	def solveInput(input:Tuple2[String, Int]):Unit = {
 		val lines = getLinesOf(s"$prefix${input._1}.txt")
 			
-		if(input._1 != "example") return
-		if(input._2 != 0) return
-		
+				
 		
 	}
 	
 	def solve():Unit = {
 		val delim = "----------------------"
 		
-		for(input <- List(("example", 0), ("input", 0), ("example2", 1), ("input", 1))) time(input._1 + " version " + input._2.toString(), "ms", {    
+		for(input <- List(
+			("example", 0),
+			("input", 0),
+			("example2", 1),
+			("input", 1)
+		)) time(input._1 + " version " + input._2.toString(), "ms", {    
 			println (s"$delim\n$packageDate ${input._1} version ${input._2} processing")
 			
 			solveInput(input)
