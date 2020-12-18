1fe9d223320cec4c62070ab4160eed1aedf8d2f9
hyperchessbot <hyperchessbot@gmail.com>
2020-12-18 07:15:15 +0000

    Day 18 Part 1

--- scala/src/main/scala/adventofcode/20201218/solution.scala
+++ scala/src/main/scala/adventofcode/20201218/solution.scala
@@ -11,9 +11,57 @@ object problem{
 	
 	val prefix = s"src/main/scala/adventofcode/$packageDate/"
 	
+	def doOp(current:Long, term:Long, op:String):Long = {		
+		op match {
+			case "+" => return (current + term)
+			case "*" => return (current * term)
+			case _ => return(term)
+		}
+		current
+	}
+	
+	def eval(setTerms:List[String], setOp:String, setCurrent:Long, level:Int):Tuple2[Long, List[String]] = {
+		var term = setTerms.head
+		var rest = setTerms.tail
+		var current = setCurrent
+		var op = setOp
+		
+		do{
+			term match {
+				case "(" => {
+					val result = eval(rest, "", current, level + 1)
+					rest = result._2
+					current = doOp(current, result._1, op)
+				}
+				case ")" => return (current, rest)
+				case "+" => op = "+"
+				case "*" => op = "*"
+				case _ => current = doOp(current, term.toLong, op)				
+			}	
+						
+			if(rest.length == 0) return (current, List())
+			
+			term = rest.head
+			rest = rest.tail
+		}while(true)		
+		
+		(current, List())
+	}
+	
 	def solveInput(input:Tuple2[String, Int]):Unit = {
 		val lines = getLinesOf(s"$prefix${input._1}.txt")
-	
+		
+		var sum = 0L
+		
+		lines.foreach(line => {
+			val terms = line.replaceAll("\\)", " )").replaceAll("\\(", "( ").split(" ").toList
+			
+			val value = eval(terms, "", 0, 0)._1
+			
+			sum += value
+			
+			println(s"$line = $value ( acc = $sum )")
+		})
 	}
 	
 	def solve():Unit = {
