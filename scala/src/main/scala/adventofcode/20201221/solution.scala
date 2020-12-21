b5284a07c77307b3c66a4de117cb42507b801357
hyperchessbot <hyperchessbot@gmail.com>
2020-12-21 06:14:57 +0000

    Day 21 Part 1

--- scala/src/main/scala/adventofcode/20201221/solution.scala
+++ scala/src/main/scala/adventofcode/20201221/solution.scala
@@ -11,10 +11,72 @@ object problem{
 	
 	val prefix = s"src/main/scala/adventofcode/$packageDate/"
 	
+	case class Food(foodStr:String){
+		var ingredients = Set[String]()
+		var allergens = Set[String]()
+		
+		foodStr match {
+			case s"$ingredientsStr (contains $allergensStr)" =>  {
+				ingredients = ingredientsStr.split(" ").toSet
+				allergens = allergensStr.split(", ").toSet
+			}
+			case _ => println("invalid food", foodStr)
+		}
+		
+		override def toString() = s"[Food $ingredients $allergens]"
+	}
+	
 	def solveInput(input:Tuple2[String, Int]):Unit = {
 		val lines = getLinesOf(s"$prefix${input._1}.txt")
 			
-	
+		if(lines.length > 0){			
+			val canBeIn = scala.collection.mutable.Map[String, Set[String]]()
+			
+			val allIngredients = scala.collection.mutable.Set[String]()
+			val allergyIngredients = scala.collection.mutable.Set[String]()
+			
+			val foods = lines.map(Food(_)).toList
+			
+			foods.foreach(food => {
+				allIngredients ++= food.ingredients				
+				
+				for(allergen <- food.allergens){
+					canBeIn.updateWith(allergen)(_ match {
+						case None => Some(food.ingredients)
+						case Some(ingredients) => {							
+							Some(ingredients.intersect(food.ingredients))
+						}
+					})
+				}
+			})
+			
+			for((_, ingredients) <- canBeIn){
+				allergyIngredients ++= ingredients
+			}
+			
+			val safeIngredients = allIngredients.diff(allergyIngredients)
+			
+			/*println("all ingredients")
+			println(allIngredients)
+			println("allergy ingredients")
+			println(allergyIngredients)
+			println("safe ingredients")
+			println(safeIngredients)*/
+			
+			var cnt = 0
+			
+			for(food <- foods){
+				for(ingredient <- food.ingredients){
+					if(safeIngredients.contains(ingredient)) cnt += 1
+				}
+			}
+			
+			if(input._2 == 0){
+				println(cnt)	
+			}
+			
+			
+		}
 	}
 	
 	def solve():Unit = {
