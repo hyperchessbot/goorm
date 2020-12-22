5d87069c0f34f5d60ff9ceffbdd399b544812cf2
hyperchessbot <hyperchessbot@gmail.com>
2020-12-22 07:27:29 +0000

    Day 22 Part 1

--- scala/src/main/scala/adventofcode/20201222/solution.scala
+++ scala/src/main/scala/adventofcode/20201222/solution.scala
@@ -11,10 +11,40 @@ object problem{
 	
 	val prefix = s"src/main/scala/adventofcode/$packageDate/"
 	
+	var playerCards = Array[Array[Int]]()
+	
+	var total = 0
+	
+	def printCards():Unit = {
+		println(playerCards(0).toList)
+		println(playerCards(1).toList)
+		println(total)
+	}
+	
+	def playRound():Boolean = {
+		val head0 = playerCards(0).head
+		val head1 = playerCards(1).head
+		val winner = if(head0 > head1) 0 else 1
+		playerCards = playerCards.map(_.tail)
+		playerCards(winner) = playerCards(winner) :++ Array(head0, head1).sorted.reverse
+		total = playerCards(winner).zipWithIndex.map(a => a._1 * ( playerCards(winner).length - a._2 ) ).sum
+		(playerCards(0).length != 0) && (playerCards(1).length != 0)
+	}
+	
 	def solveInput(input:Tuple2[String, Int]):Unit = {
 		val lines = getLinesOf(s"$prefix${input._1}.txt")
+		
+		if(lines.length > 0){
+		
+			//if(input._1 != "example") return
+
+			playerCards = lines.mkString("\n").split("\n\n").map(_.split("\n").tail.map(_.toInt))
+
+			while(playRound()){}
+
+			printCards()
 			
-	
+		}
 	}
 	
 	def solve():Unit = {
