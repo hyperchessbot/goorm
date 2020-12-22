5e1c261f84988d2d639c3de532d783bbf3f52a43
hyperchessbot <hyperchessbot@gmail.com>
2020-12-22 12:15:51 +0000

    Prepare Day 23

---
+++ scala/src/main/scala/adventofcode/20201223/solution.scala
@@ -0,0 +1,31 @@
+package adventofcode20201223
+
+import scala.language.postfixOps
+import utils._
+import table._
+
+object problem{	
+	println(s"package name ${problem.getClass.getName.split("\\.")(0)}")
+	
+	val packageDate = "([0-9]+)".r.findFirstMatchIn(problem.getClass.getName).get
+	
+	val prefix = s"src/main/scala/adventofcode/$packageDate/"
+	
+	def solveInput(input:Tuple2[String, Int]):Unit = {
+		val lines = getLinesOf(s"$prefix${input._1}.txt")
+			
+	
+	}
+	
+	def solve():Unit = {
+		val delim = "----------------------"
+		
+		for(input <- List(("example", 0), ("input", 0), ("example2", 1), ("input", 1))) time(input._1 + " version " + input._2.toString(), "ms", {    
+			println (s"$delim\n$packageDate ${input._1} version ${input._2} processing")
+			
+			solveInput(input)
+		})
+		
+		println(delim)
+	}
+}
